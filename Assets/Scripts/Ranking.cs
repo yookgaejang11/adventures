60d0899d31882fbae28c3085c5f79793071bf071
yookgaejang11 <yookprograming@gmail.com>
2025-03-28 14:42:38 +0900

    랭킹 정렬

--- Assets/Scripts/Ranking.cs
+++ Assets/Scripts/Ranking.cs
@@ -1,14 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Ranking : MonoBehaviour
 {
-    public List<GameObject> tests = new List<GameObject>();
+    public List<Text> rankList = new List<Text>();
+    public List<float> clearTime = new List<float>();
     // Start is called before the first frame update
     void Start()
     {
-        tests[2].transform.SetSiblingIndex(0);
+        clearTime.Sort();
+
+        for (int i = 0; i < rankList.Count; i++)
+        {
+            rankList[i].text = (i+1)+ ". " + clearTime[i].ToString();
+        }
+        
     }
 
     // Update is called once per frame
